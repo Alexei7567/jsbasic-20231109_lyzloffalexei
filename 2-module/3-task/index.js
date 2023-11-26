9720456182fe2cbc65d0655a7ff0d7d5c56dab7a
Alexei7567 <151124532+Alexei7567@users.noreply.github.com>
2023-11-26 16:13:10 +0300

    Для сложения чисел, пробегаемся по объекту с помощью цикла for in, дальше проверяем значения на число, а также чтобы значение не было Nan или Infinity.

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,5 +1,14 @@
 let calculator = {
-  // ваш код
+  sum() {
+      return this.a + this.b
+  },
+  mul() {
+      return this.a * this.b
+  },
+  read: function(a, b){
+      this.a = a;
+      this.b = b;
+  }
 };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
